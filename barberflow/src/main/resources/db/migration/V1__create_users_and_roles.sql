c75b45a598a20a7cf9dc12bbe75d75688f612aba
Gianmarcx <sorrentinogianmarco179@gmail.com>
2026-05-13 22:53:03 +0200

    backend

--- barberflow/src/main/resources/db/migration/V1__create_users_and_roles.sql
+++ barberflow/src/main/resources/db/migration/V1__create_users_and_roles.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
   id BIGINT AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(255) NOT NULL UNIQUE,
-  password VARCHAR(255) NOT NULL
+  password VARCHAR(255) NOT NULL,
    name VARCHAR(255),      
   surname VARCHAR(255)    
 );
